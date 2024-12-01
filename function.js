75ebf6313a599075fbbb1ec39a66389e8d1fde41
ankit-532 <as7090929@gmail.com>
2024-12-01 14:19:39 +0530

    html done

---
+++ function.js
@@ -0,0 +1,19 @@
+// //named funcion
+// function func(a) {
+//     console.log(a);
+// }
+
+
+// let a =5;
+// func(a);
+
+// named fucntion eith return type
+
+function sum(a,b){
+    return a+b;
+}
+
+let a=4;
+let b=3;
+let c=sum(a,b);
+console.log(c);
---
+++ script.js
@@ -0,0 +1,17 @@
+// // let var const
+// var a ="hello";
+// console.log(a);
+
+// var a="world";
+// console.log(a);
+
+// let a ="hello";
+// console.log(a);
+
+// a="world";
+// console.log(a);
+
+const a="hello";
+console.log(a);
+a="eold";
+console.log(a);
